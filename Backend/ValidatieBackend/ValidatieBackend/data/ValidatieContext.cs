e3984ce7a1cf3b1de2a95ed51957fc7847a29cab
Joeri Lieuw <joeri@xprtz.net>
2020-04-11 16:10:52 +0200

    updated gitignore

--- Backend/ValidatieBackend/ValidatieBackend/data/ValidatieContext.cs
+++ Backend/ValidatieBackend/ValidatieBackend/data/ValidatieContext.cs
@@ -8,7 +8,11 @@ namespace ValidatieBackend.data
             : base(options)
         {
         }
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // seed
+            modelBuilder.Entity<Adres>().HasData(new Adres { Id = 1, Straat = "Blastraat", PostCode = "9999AA", Huisnummer = 39, Gemeente = "Blabla" });
+        }
         public DbSet<Adres> Adressen { get; set; }
     }
 }
